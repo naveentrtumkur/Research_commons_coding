21162d6e6c508f1a9b49d095c48b829e834883fb
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-06-11 18:40:30 -0400

    Interesection of two lists
    Intersection of two linkled lists

---
+++ intersection_two_lists.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        ListNode curA = headA, curB = headB;
+        
+        if(curA == null || curB == null)
+            return null;
+        
+        while(curA != curB)
+        {
+            curA = (curA == null)?headB:curA.next;
+            
+            curB = (curB == null)?headA:curB.next;
+            
+            
+            
+        }
+        
+        return curA;
+        
+        
+    }
+}
